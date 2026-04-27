c184967964ac06e9539f20c0145068cdd47c9f42
GinoGr <ginograndin@hotmail.com>
2026-04-26 17:55:25 -0700

    Fix: Added protected overrid to dbcontext

--- LibraryBookBorrowingSystm/Data/ApplicationDbContext.cs
+++ LibraryBookBorrowingSystm/Data/ApplicationDbContext.cs
@@ -1,13 +1,24 @@
-using LibraryBookBorrowingSystm.Models;
 using Microsoft.EntityFrameworkCore;
+using LibraryBookBorrowingSystm.Models;
 
-namespace LibraryBookBorrowingSystm.Data;
-
-public class ApplicationDbContext : DbContext
+namespace LibraryBookBorrowingSystm.Data
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
 
-    public DbSet<Book> Books { get; set; }
-    public DbSet<Member> Members { get; set; }
-    public DbSet<BorrowRecord> BorrowRecords { get; set; }
+        public DbSet<Member> Members => Set<Member>();
+	public DbSet<Book> Books => Set<Book>();
+	public DbSet<BorrowRecord> BorrowRecords => Set<BorrowRecord>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Member>().ToTable("Members");
+	    modelBuilder.Entity<Book>().ToTable("Books");
+	    modelBuilder.Entity<BorrowRecord>().ToTable("BorrowRecords");
+        }
+    }
 }
